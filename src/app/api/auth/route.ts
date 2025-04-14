3dc88e41a8c265f0904c8d172d9edd4342a42995
Dante Martinez <slothgeneral331@gmail.com>
2025-04-14 11:43:13 -0400

    login and register: dante

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,18 @@
+import connectMongoDB from "../../../../config/mongodb";
+import User from "@/app/models/userSchema";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { username, password } = await request.json();
+  await connectMongoDB();
+ 
+  const user = await User.findOne({username});
+  if (!user) return NextResponse.json({ message: "Invalid credentials" });
+
+  const isMatch = await (password == user.password);
+  if (!isMatch) return NextResponse.json({message: "Invalid Credentials" });
+
+  return NextResponse.json({ message: "User logged in successfully" }, { status: 201 });
+  
+}
